ab0d5f7a74b69f4e192a4f09c0119b0ab027e962
shreyaprabhu19 <shreyaprabhu77@gmail.com>
2022-04-22 21:43:53 +0530

    updated functions.py

--- functions.py
+++ functions.py
@@ -19,40 +19,42 @@ class GHRSS:
         self.colors1 = []
         self.colors2 = []
         # colors can changed as per output requirement
-        self.colors_init = ['r', 'g', 'y', 'k', 'violet']
+        self.colors_init = ["r", "g", "y", "k", "violet"]
         self.coordinates = 0
-        self.coord_ra = 0
-        self.coord_dec = 0
-        self.coord_l = 0
-        self.coord_b = 0
+        self.coord_ra = []
+        self.coord_dec = []
+        self.coord_l = []
+        self.coord_b = []
         self.radec = 0
-        self.label = []
+        self.lb = 0
+        self.label_RaDec = []
+        self.label_lb = []
         self.color_count = 0
 
     # writes the colors exisiting in list colors1 and colors 2
     def write_colors(self, color_new):
         # color map can be changed according to output requirement
         self.colors2 = iter(cm.Blues(numpy.linspace(0, 1, 6)))
-        with open('colors.txt', 'w') as f:
+        with open("colors.txt", "w") as f:
             for c in color_new:
                 f.write("%s\n" % c)
         f.close()
         x = str(self.color_count)
-        with open('colors2.txt', 'w') as f1:
-            f1.write('%s' % x)
+        with open("colors2.txt", "w") as f1:
+            f1.write("%s" % x)
         f1.close()
 
     def read_colors(self):
         color1 = []
-        with open("colors.txt", 'r') as file:
+        with open("colors.txt", "r") as file:
             temp = file.readlines()
         for i in temp:
-            temp2 = i.split('\n')
+            temp2 = i.split("\n")
             color1.append(temp2[0])
         self.colors1 = color1
         # color map can be changed
         self.colors2 = iter(cm.Blues(numpy.linspace(0, 1, 6)))
-        with open("colors2.txt", 'r') as file:
+        with open("colors2.txt", "r") as file:
             temp = file.readlines()
         temp2 = int(temp[0])
         while temp2:
@@ -67,7 +69,7 @@ class GHRSS:
         s = {}
 
         for i in range(0, len(file_names)):
-            if split_char == 'J':
+            if split_char == "J":
                 header_index = header[0]
             else:
                 header_index = header[i]
@@ -101,13 +103,18 @@ class GHRSS:
         for i in range(0, len(files)):
             lat = []
             lon = []
-            coord1 = SkyCoord(self.RA_source[i], self.Dec_source[i], unit=(u.hourangle, u.deg))
+            coord1 = SkyCoord(
+                self.RA_source[i], self.Dec_source[i], unit=(u.hourangle, u.deg)
+            )
             coord1 = coord1.galactic
             L = coord1.l.wrap_at(180 * u.degree).radian
             B = coord1.b.wrap_at(180 * u.degree).radian
             c = next(self.colors2)
             for j, k in zip(B, L):
-                if j < coord.Angle(-5, u.degree).radian or j > coord.Angle(5, u.degree).radian:
+                if (
+                    j < coord.Angle(-5, u.degree).radian
+                    or j > coord.Angle(5, u.degree).radian
+                ):
                     lat.append(j)
                     lon.append(k)
             ax.scatter(lon, lat, color=c, label=files[i])
@@ -118,7 +125,9 @@ class GHRSS:
     # These are then plotted using different symbols, same color for a given file.
     def plot_discovered(self, file_d, phase, n2, ax):
         for i in range(0, len(file_d)):
-            coord1 = SkyCoord(self.RA_objects[i], self.Dec_objects[i], unit=(u.hourangle, u.deg))
+            coord1 = SkyCoord(
+                self.RA_objects[i], self.Dec_objects[i], unit=(u.hourangle, u.deg)
+            )
             coord1 = coord1.galactic
             L = coord1.l.wrap_at(180 * u.degree).radian
             B = coord1.b.wrap_at(90 * u.degree).radian
@@ -130,7 +139,7 @@ class GHRSS:
             lon_msp = []
             lon_normalpulsar = []
             for x in L:
-                p = phase[file_d[i]]['Period'][k]
+                p = phase[file_d[i]]["Period"][k]
                 if float(p) < 0:
                     lat_rrat.append(L[k])
                     lon_rrat.append(B[k])
@@ -142,11 +151,30 @@ class GHRSS:
                     lon_normalpulsar.append(B[k])
                 k = k + 1
             if len(lat_rrat) != 0:
-                ax.scatter(lat_rrat, lon_rrat, color=self.colors1[0], marker='^', s=80, label='RRATs discovered in ' + file_d[i])
+                ax.scatter(
+                    lat_rrat,
+                    lon_rrat,
+                    color=self.colors1[0],
+                    marker="^",
+                    s=80,
+                    label="RRATs discovered in " + file_d[i],
+                )
             if len(lat_normalpulsar) != 0:
-                ax.scatter(lat_normalpulsar, lon_normalpulsar, color=self.colors1[0], label='Pulsars discovered in ' + file_d[i])
+                ax.scatter(
+                    lat_normalpulsar,
+                    lon_normalpulsar,
+                    color=self.colors1[0],
+                    label="Pulsars discovered in " + file_d[i],
+                )
             if len(lat_msp) != 0:
-                ax.scatter(lat_msp, lon_msp, color=self.colors1[0], marker='*', s=80, label='MSPs discovered in ' + file_d[i])
+                ax.scatter(
+                    lat_msp,
+                    lon_msp,
+                    color=self.colors1[0],
+                    marker="*",
+                    s=80,
+                    label="MSPs discovered in " + file_d[i],
+                )
             self.colors1.pop(0)
 
     # Calls the function to read data files containing the list of sources
@@ -154,7 +182,7 @@ class GHRSS:
     def read_and_plot_sources(self, file_names, ax):
         data_table, n1, header = function.read_data(file_names, header=True)
         if n1 != 0:
-            self.set_coordinates(file_names, data_table, header, split_char='_')
+            self.set_coordinates(file_names, data_table, header, split_char="_")
             self.plot_area(file_names, n1, ax)
 
     # Calls the function to read data files containing the list of pulsar names and period (.csv files)
@@ -162,128 +190,146 @@ class GHRSS:
     def read_and_plot_objects(self, file_d, ax):
         data_table, n2, header = function.read_data(file_d, header=False)
         if data_table:
-            self.set_coordinates(file_d, data_table, header, split_char='J')
+            self.set_coordinates(file_d, data_table, header, split_char="J")
             self.plot_discovered(file_d, data_table, n2, ax)
 
-    # calls the function to plot given data files and saves the plot object by pickeling it.
+    # calls the function to plot given data files and saves the plot object by pickling it.
     # It shows the final plot after the program is terminated.
     def show_plot(self, ax):
-        if len(self.file_names_sources) !=0:
+        if len(self.file_names_sources) != 0:
             self.read_and_plot_sources(self.file_names_sources, ax)
         if len(self.file_names_objects) != 0:
             self.read_and_plot_objects(self.file_names_objects, ax)
         if self.coordinates == 1:
             self.plot_using_coordinate(ax)
-        ax.legend(loc="upper right", bbox_to_anchor=(1, 1.15), ncol=2, prop={'size': 10})
+        ax.legend(
+            loc="upper right", bbox_to_anchor=(1, 1.15), ncol=2, prop={"size": 10}
+        )
         function.save_ax(ax)
+        plt.savefig("GHRSS.png", format="png", dpi=150)
         plt.show()
 
     def accept_coord(self):
-        self.label = input("enter the name: ")
         n = input("Enter 1.RA/Dec or 2.Latitude-longitude: ")
-        if n == '1':
-            self.coord_ra = float(input("Enter RA"))
-            self.coord_dec = float(input('Enter declination'))
+        if n == "1":
+            self.label_RaDec.append(input("enter the name: "))
+            self.coord_ra.append(float(input("Enter RA")))
+            self.coord_dec.append(float(input("Enter declination")))
             self.radec = 1
-        elif n == '2':
-            self.coord_l = float(input("Enter longitude"))
-            self.coord_b = float(input('Enter latitude'))
+        elif n == "2":
+            self.label_lb.append(input("enter the name: "))
+            self.coord_l.append(float(input("Enter longitude")))
+            self.coord_b.append(float(input("Enter latitude")))
+            self.lb = 1
 
     def plot_using_coordinate(self, ax):
-        if self.radec == 1:
-            coord1 = SkyCoord(self.coord_ra, self.coord_dec, unit=(u.hourangle, u.deg))
+        j = 0
+        for i in self.label_RaDec:
+            coord1 = SkyCoord(
+                self.coord_ra[j], self.coord_dec[j], unit=(u.hourangle, u.deg)
+            )
             coord1 = coord1.galactic
             L = coord1.l.wrap_at(180 * u.degree).radian
             B = coord1.b.wrap_at(180 * u.degree).radian
-            ax.scatter(L, B, color=self.colors1[0], s=80, label=self.label)
+            ax.scatter(L, B, color=self.colors1[0], s=80, label=self.label_RaDec[j])
+            j = j + 1
             self.colors1.pop(0)
-        else:
-            L= coord.Angle(self.coord_l, u.degree)
-            B = coord.Angle(self.coord_b, u.degree)
+        j = 0
+        for i in self.label_lb:
+            L = coord.Angle(self.coord_l[j], u.degree)
+            B = coord.Angle(self.coord_b[j], u.degree)
             L = L.wrap_at(180 * u.degree).radian
             B = B.wrap_at(180 * u.degree).radian
-            ax.scatter(L, B, color=self.colors1[0], s=80, label=self.label)
+            ax.scatter(L, B, color=self.colors1[0], s=80, label=self.label_lb[j])
+            j = j + 1
             self.colors1.pop(0)
 
     # accepts the user's choice and depending on it, either calls the function to create new figure and plot object 'ax'
     # and initialises the colors or use the pickeled object and existing colors.
     def accept_choice(self):
-        ch = input('1.Plot with all the option\n2.Plot on the existing graph\nEnter 1 or 2: ')
-        if ch == '1':
+        ch = input(
+            "1.Plot with all the option\n2.Plot on the existing graph\nEnter 1 or 2: "
+        )
+        if ch == "1":
             ax = self.create_ax()
             self.write_colors(self.colors_init)
             self.read_colors()
             self.user_choice()
             self.show_plot(ax)
             self.write_colors(self.colors1)
-        elif ch == '2':
+        elif ch == "2":
             self.read_colors()
-            with open('myplot.pkl','rb') as fid:
+            with open("myplot.pkl", "rb") as fid:
                 ax1 = pickle.load(fid)
             self.user_choice()
             self.show_plot(ax1)
             self.write_colors(self.colors1)
         else:
-            print('invalid input')
+            print("invalid input")
 
     def user_choice(self):
-        plot_choice = '0'
-        while plot_choice != '4':
-            plot_choice = input("Plot\n1.Targeted sky area\n2.Observed sources\n3.Discovered object\n4.end")
-            if plot_choice == '4':
+        plot_choice = "0"
+        while plot_choice != "4":
+            plot_choice = input(
+                "Plot\n1.Targeted sky area\n2.Observed sources\n3.Discovered object\n4.end"
+            )
+            if plot_choice == "4":
                 return 0
-            elif plot_choice == '1':
-                x = int(input('Enter the number of files to plot targetted file: '))
+            elif plot_choice == "1":
+                x = int(input("Enter the number of files to plot targetted file: "))
                 for i in range(x):
-                    self.file_names_sources.append(input('Enter the file names: '))
+                    self.file_names_sources.append(input("Enter the file names: "))
                     # file with extension .list, having values of ra-dec starting with 'HR_'
-            elif plot_choice == '2':
-                print("Observed sources:\n1.Plot all observed\n2.Plot different data files")
+            elif plot_choice == "2":
+                print(
+                    "Observed sources:\n1.Plot all observed\n2.Plot different data files"
+                )
                 co = input("enter 1 or 2: ")
-                if co == '1':
-                    if input("append more to existing data?(y/n): ") == 'y':
+                if co == "1":
+                    if input("append more to existing data?(y/n): ") == "y":
                         y = input("Enter the file name: ")
-                        x = y + '.list'
+                        x = y + ".list"
                         # file with extension .list, having values of ra-dec starting with 'HR_'
-                        function.append_file('GHRSS observed sources.list', x)
-                        self.file_names_sources.append('GHRSS observed sources')
+                        function.append_file("GHRSS observed sources.list", x)
+                        self.file_names_sources.append("GHRSS observed sources")
                     else:
-                        self.file_names_sources.append('GHRSS observed sources')
-                elif co == '2':
-                    n = int(input('Enter the number of files to plot: '))
+                        self.file_names_sources.append("GHRSS observed sources")
+                elif co == "2":
+                    n = int(input("Enter the number of files to plot: "))
                     for i in range(n):
-                        self.file_names_sources.append(input('Enter the file name: '))
+                        self.file_names_sources.append(input("Enter the file name: "))
                         # file with extension .list, having values of ra-dec starting with 'HR_'
                 else:
-                    print('invalid input')
+                    print("invalid input")
 
-                print('\n')
-            elif plot_choice == '3':
+                print("\n")
+            elif plot_choice == "3":
                 dp = input(
-                    "Plot\n1.Pulsars dicovered in phase-1\n2.Pulsars discovered in phase-2\n3.data files containing coordinates\n4.by giving coordiantes")
-                if dp == '1':
-                    if input('append new phase1 data?(y/n)') == 'y':
+                    "Plot\n1.Pulsars dicovered in phase-1\n2.Pulsars discovered in phase-2\n3.data files containing coordinates\n4.by giving coordiantes"
+                )
+                if dp == "1":
+                    if input("append new phase1 data?(y/n)") == "y":
                         y = input("Enter the file name: ")
-                        x = y + '.csv'
+                        x = y + ".csv"
                         # .csv file with pulsar_name and period values(no header)
-                        function.append_file('phase1.csv', x)
-                        self.file_names_objects.append('phase1')
+                        function.append_file("phase1.csv", x)
+                        self.file_names_objects.append("phase1")
                     else:
-                        self.file_names_objects.append('phase1')
-                elif dp == '2':
-                    if input('append new phase2 data?(y/n)') == 'y':
+                        self.file_names_objects.append("phase1")
+                elif dp == "2":
+                    if input("append new phase2 data?(y/n)") == "y":
                         y = input("Enter the file name: ")
-                        x = y + '.csv'
+                        x = y + ".csv"
                         # .csv file with pulsar_name and period values(no header)
-                        function.append_file('phase2.csv', x)
-                        self.file_names_objects.append('phase2')
+                        function.append_file("phase2.csv", x)
+                        self.file_names_objects.append("phase2")
                     else:
-                        self.file_names_objects.append('phase2')
-                elif dp == '3':
+                        self.file_names_objects.append("phase2")
+                elif dp == "3":
                     file_name = input("enter the name of the file: ")
                     # .csv file with pulsar_name and period values(header:'Pulsar_name,Period')
                     self.file_names_objects.append(file_name)
-                elif dp == '4':
+                elif dp == "4":
                     self.coordinates = 1
                     self.accept_coord()
                 else:
@@ -296,7 +342,6 @@ class GHRSS:
         fig = plt.figure(figsize=(15, 15))
         ax = fig.add_subplot(111, projection="aitoff")
         ax.grid()
-        ax.set_ylabel('Galactic latitude, b', fontsize=15)
-        ax.set_xlabel('Galactic longitude, l', fontsize=15)
+        ax.set_ylabel("Galactic latitude, b", fontsize=15)
+        ax.set_xlabel("Galactic longitude, l", fontsize=15)
         return ax
-
